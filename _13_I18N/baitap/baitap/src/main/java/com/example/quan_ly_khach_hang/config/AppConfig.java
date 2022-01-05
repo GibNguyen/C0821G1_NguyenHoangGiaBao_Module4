3c8060265820c318d9c5e80e782694dde11944a9
GibNguyen <89565115+GibNguyen@users.noreply.github.com>
2022-01-05 20:54:34 +0700

    5/1

--- _13_I18N/baitap/baitap/src/main/java/com/example/quan_ly_khach_hang/config/AppConfig.java
+++ _13_I18N/baitap/baitap/src/main/java/com/example/quan_ly_khach_hang/config/AppConfig.java
@@ -3,6 +3,7 @@ package com.example.quan_ly_khach_hang.config;
 import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -12,12 +13,13 @@ import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 import java.util.Locale;
 
-@Configurable
+@Configuration
 public class AppConfig implements WebMvcConfigurer {
     @Bean
     public MessageSource messageSource(){
         ResourceBundleMessageSource messageSource=new ResourceBundleMessageSource();
         messageSource.setBasename("msg/language");
+        messageSource.setDefaultEncoding("UTF-8");
         return messageSource;
     }
     @Override
